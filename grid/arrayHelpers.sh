d5e570494f532f7df4a666544234c510b08d6295
daniel.lang <daniel.lang@helmholtz-muenchen.de>
2016-07-16 10:36:34 +0200

    Added function to check if array contains a string
    return code is 0 i.e. true in if ()

--- grid/arrayHelpers.sh
+++ grid/arrayHelpers.sh
@@ -9,3 +9,8 @@ tablerow2array() {
 	IFS=";" read -ra array <<< "$line"
 	export array
 }
+containsElement () {
+  local e
+  for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
+  return 1
+}
